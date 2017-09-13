bb02a6440063186038ec8bd125c4b0695e73d8dd
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-09-13 10:59:30 +0200

    add new file

--- mini_chat.php
+++ mini_chat.php
@@ -40,8 +40,8 @@
 <input type="text" name="message" value="message">
 <input type="submit" name="envoyer" value="">
 </form>
-
 </div>
+
     <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
 
